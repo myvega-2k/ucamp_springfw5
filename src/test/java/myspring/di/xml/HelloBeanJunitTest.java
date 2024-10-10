ff6e72ea9e8ccd924b249480a6114ce8e737ea7e
myvega-2k <myvega2k@gmail.com>
2024-10-10 14:12:59 +0900

    assertSame add

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,5 +1,6 @@
 package myspring.di.xml;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -11,5 +12,12 @@ public class HelloBeanJunitTest {
 		//1. Container Object 생성
 		ApplicationContext container = 
 				new GenericXmlApplicationContext("classpath:spring_di.xml"); 
+		
+		//2. Container에게 SpringBean 요청하기
+		Hello hello1 = (Hello)container.getBean("hello");
+		Hello hello2 = container.getBean("hello", Hello.class);
+		System.out.println(hello1 == hello2);
+		//SpringBean의 주소 비교 - assertSame()
+		assertSame(hello1, hello2);
 	}
 }
