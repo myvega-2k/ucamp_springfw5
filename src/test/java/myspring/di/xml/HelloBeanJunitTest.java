a3f579b325cca4610609ed4e64f9568b65cac452
myvega-2k <myvega2k@gmail.com>
2024-10-10 14:39:44 +0900

    StringPrinter getBean

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,23 +1,36 @@
 package myspring.di.xml;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloBeanJunitTest {
+	ApplicationContext container;
+	
+	@BeforeEach
+	void setup() {
+		//1. Container Object 생성
+		container = new GenericXmlApplicationContext("classpath:spring_di.xml"); 		
+	}
 
 	@Test
-	void helloBean() {
-		//1. Container Object 생성
-		ApplicationContext container = 
-				new GenericXmlApplicationContext("classpath:spring_di.xml"); 
-		
-		//2. Container에게 SpringBean 요청하기
+	void helloBean() {	
+		//2. Container에게 Hello SpringBean 요청하기
 		Hello hello1 = (Hello)container.getBean("hello");
 		Hello hello2 = container.getBean("hello", Hello.class);
 		System.out.println(hello1 == hello2);
 		//SpringBean의 주소 비교 - assertSame()
 		assertSame(hello1, hello2);
+		//setName setter injection 체크
+		assertEquals("Hello 스프링", hello1.sayHello());
+		//setPrinter setter injection 체크
+		hello1.print();
+		
+		//3. Container에게 StringPrinter SpringBean 요청하기
+		Printer printer = container.getBean("strPrinter", Printer.class);
+		assertEquals("Hello 스프링", printer.toString());
 	}
 }
