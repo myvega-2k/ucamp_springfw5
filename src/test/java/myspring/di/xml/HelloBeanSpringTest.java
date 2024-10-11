660acf67c8b8b28b7f588c1d7f6b03f258f37c8e
myvega-2k <myvega2k@gmail.com>
2024-10-11 09:34:12 +0900

    springtest update

--- src/test/java/myspring/di/xml/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/xml/HelloBeanSpringTest.java
@@ -1,11 +1,35 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.annotation.Resource;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_di.xml")
 public class HelloBeanSpringTest {
-
+	@Autowired
+	//@Qualifier("helloC")
+	Hello hello;
+	
+	@Resource(name = "helloC")
+	Hello helloCons;
+	
+	@Test
+	void helloBean() {
+		System.out.println(hello.sayHello());
+		System.out.println(helloCons.sayHello());
+		assertEquals("Hello 생성자", helloCons.sayHello());
+		
+		helloCons.print();
+		
+	}
+	
+	
 }
