48b37cc51286c3a6469419ddd13628e744dbbd4e
myvega-2k <myvega2k@gmail.com>
2024-10-11 11:40:36 +0900

    @Configuration Test add

--- src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -1,9 +1,15 @@
 package myspring.di.annot.config;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import myspring.di.annot.HelloBean;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = HelloBeanConfig.class, 
@@ -13,5 +19,13 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
  * 변경된 컨테이너 객체를 로드 해주는 역할을 하는 AnnotationConfigContextLoader 클래스를 loader로 설정하는 것임
  */
 public class HelloBeanConfigTest {
-
+	@Autowired
+	HelloBean helloBean;
+	
+	@Test
+	void helloConfig() {
+		assertEquals("어노테이션", helloBean.sayHello());
+	}
+	
+	
 }
